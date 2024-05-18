0e8435aa833384bb64b1681df58057d54ae19347
animusanima <molockokannzocko@gmail.com>
2024-05-18 15:33:05 +0200

    Added deleting a recipe in the services

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -33,6 +33,7 @@ export class RecipeEditComponent implements OnInit {
     } else {
       this.recipeService.addRecipe(this.recipeForm.value);
     }
+    this.onAbortEdit();
   }
 
   onAbortEdit() {
