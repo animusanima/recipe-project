59bebc84126fa65790c6208161e71fc6d8d14d4b
animusanima <molockokannzocko@gmail.com>
2024-05-26 09:12:58 +0200

    Extracted error handling into an own method in the auth.service.ts
    - Added handleError method
    - Added additional switch-cases for error handling in signup and login
    - Added a model for storing user information after signup
    - Added handleAuthentication method

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from "@angular/core";
 import {HttpClient, HttpErrorResponse} from "@angular/common/http";
-import {catchError} from "rxjs/operators";
-import {throwError} from "rxjs";
+import {catchError, tap} from "rxjs/operators";
+import {Observable, Subject, throwError} from "rxjs";
+import {User} from "./user.model";
 
 export interface AuthResponseData {
   kind: string,
@@ -17,13 +18,16 @@ export interface AuthResponseData {
   providedIn: 'root'
 })
 export class AuthService {
+  userSubject: Subject<User> = new Subject<User>();
+
   private SIGNUP_AUTH_URL: string = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBVkY1pub67h1mVPpCWmY5pjSkshkPEgHs"
   private LOGIN_AUTH_URL: string = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBVkY1pub67h1mVPpCWmY5pjSkshkPEgHs";
 
   constructor(private http: HttpClient) {
+
   }
 
-  signup(email: string, password: string) {
+  signup(email: string, password: string): Observable<AuthResponseData> {
     return this.http.post<AuthResponseData>(`${this.SIGNUP_AUTH_URL}`,
       {
         email: email,
@@ -31,18 +35,43 @@ export class AuthService {
         returnSecureToken: true
       }
     ).pipe(catchError(errorRes => {
-      return this.handleError(errorRes);
-    }));
+        return this.handleError(errorRes);
+      }),
+      tap((response: AuthResponseData): void => {
+        this.handleAuthentication(
+          response.email,
+          response.localId,
+          response.idToken,
+          +response.expiresIn);
+      }));
   }
 
-  login(email: string, password: string) {
+  login(email: string, password: string): Observable<AuthResponseData> {
     return this.http.post<AuthResponseData>(`${this.LOGIN_AUTH_URL}`, {
       email: email,
       password: password,
       returnSecureToken: true
     }).pipe(catchError(errorRes => {
-      return this.handleError(errorRes);
-    }));
+        return this.handleError(errorRes);
+      }),
+      tap((response: AuthResponseData): void => {
+        this.handleAuthentication(
+          response.email,
+          response.localId,
+          response.idToken,
+          +response.expiresIn);
+      }));
+  }
+
+  private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+    const expirationDate: Date = new Date(new Date().getTime() + (expiresIn * 1000));
+    const user = new User(
+      email,
+      userId,
+      token,
+      expirationDate
+    );
+    this.userSubject.next(user);
   }
 
   private handleError(errorResponse: HttpErrorResponse) {
@@ -52,38 +81,40 @@ export class AuthService {
       status: 500,
     };
 
-    if (!errorResponse.error || !errorResponse.error.error) {
+    if (!errorResponse.error) {
       return throwError(error);
     }
 
-    switch (errorResponse.error.error.message) {
-      case 'EMAIL_NOT_FOUND':
-        error.message = 'E-Mail address is not correct';
-        error.detailMessage = 'The user could have been deleted by you or an administrator';
-        error.status = 401;
-        break;
-      case 'INVALID_LOGIN_CREDENTIALS':
-        error.message = 'Password invalid or no password exists for the given email';
-        error.detailMessage = 'Please check the given email and password for correctness.';
-        error.status = 401;
-        break;
-      case 'EMAIL_EXISTS':
-        error.message = 'The email already exists';
-        error.status = 401;
-        break;
-      case 'OPERATION_NOT_ALLOWED':
-        error.message = 'You are not allowed to sign up';
-        error.status = 403;
-        break;
-      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
-        error.message = 'Too many requests sent. Please try again later.';
-        error.status = 500;
-        break;
-      case 'USER_DISABLED':
-        error.message = 'User account not accessible';
-        error.detailMessage = 'The account have been disabled by an administrator.';
-        error.status = 403;
-        break;
+    if (errorResponse.error.error?.message) {
+      switch (errorResponse.error.error.message) {
+        case 'EMAIL_NOT_FOUND':
+          error.message = 'E-Mail address is not correct';
+          error.detailMessage = 'The user could have been deleted by you or an administrator';
+          error.status = 401;
+          break;
+        case 'INVALID_LOGIN_CREDENTIALS':
+          error.message = 'Password invalid or no password exists for the given email';
+          error.detailMessage = 'Please check the given email and password for correctness.';
+          error.status = 401;
+          break;
+        case 'EMAIL_EXISTS':
+          error.message = 'The email already exists';
+          error.status = 401;
+          break;
+        case 'OPERATION_NOT_ALLOWED':
+          error.message = 'You are not allowed to sign up';
+          error.status = 403;
+          break;
+        case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+          error.message = 'Too many requests sent. Please try again later.';
+          error.status = 500;
+          break;
+        case 'USER_DISABLED':
+          error.message = 'User account not accessible';
+          error.detailMessage = 'The account have been disabled by an administrator.';
+          error.status = 403;
+          break;
+      }
     }
 
     return throwError(error);
---
+++ src/app/auth/user.model.ts
@@ -0,0 +1,21 @@
+export class User {
+  constructor(
+    public email: string,
+    public id: string,
+    private _token: string,
+    private _tokenExpirationDate: Date) {
+
+  }
+
+  get token() {
+    if (!this.tokenExpirationDate() || new Date() > this.tokenExpirationDate()) {
+      return null;
+    }
+    return this.token.clone();
+  }
+
+  get tokenExpirationDate() {
+    return this.tokenExpirationDate.clone();
+  }
+
+}
