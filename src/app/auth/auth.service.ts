8b23bca6b6b975ec98020f750ce73c938ca023dc
animusanima <molockokannzocko@gmail.com>
2024-05-26 08:39:59 +0200

    Extracted error handling into an own method in the auth.service.ts
    - Added handleError method
    - Added additional switch-cases for error handling in signup and login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {catchError} from "rxjs/operators";
 import {throwError} from "rxjs";
 
@@ -31,32 +31,7 @@ export class AuthService {
         returnSecureToken: true
       }
     ).pipe(catchError(errorRes => {
-      let error = {
-        message: "An error occurred while signing up.",
-        detailMessage: 'Please try again later',
-        status: 500,
-      };
-
-      if (!errorRes.error || !errorRes.error.error) {
-        return throwError(error);
-      }
-
-      switch (errorRes.error.error.message) {
-        case 'EMAIL_EXISTS':
-          error.message = 'The email already exists';
-          error.status = 401
-          break;
-        case 'OPERATION_NOT_ALLOWED':
-          error.message = 'You are not allowed to sign up';
-          error.status = 403
-          break;
-        case 'TOO_MANY_ATTEMPTS_TRY_LATER':
-          error.message = 'Too many requests sent. Please try again later.';
-          error.status = 500
-          break;
-      }
-
-      return throwError(error);
+      return this.handleError(errorRes);
     }));
   }
 
@@ -66,32 +41,51 @@ export class AuthService {
       password: password,
       returnSecureToken: true
     }).pipe(catchError(errorRes => {
-      let error = {
-        message: "An error occurred while signing up.",
-        detailMessage: 'Please try again later',
-        status: 500,
-      };
-
-      if (!errorRes.error || !errorRes.error.error) {
-        return throwError(error);
-      }
-
-      switch (errorRes.error.error.message) {
-        case 'EMAIL_EXISTS':
-          error.message = 'The email already exists';
-          error.status = 401
-          break;
-        case 'OPERATION_NOT_ALLOWED':
-          error.message = 'You are not allowed to sign up';
-          error.status = 403
-          break;
-        case 'TOO_MANY_ATTEMPTS_TRY_LATER':
-          error.message = 'Too many requests sent. Please try again later.';
-          error.status = 500
-          break;
-      }
-
-      return throwError(error);
+      return this.handleError(errorRes);
     }));
+  }
+
+  private handleError(errorResponse: HttpErrorResponse) {
+    let error = {
+      message: "An error occurred while signing up.",
+      detailMessage: 'Please try again later',
+      status: 500,
+    };
+
+    if (!errorResponse.error || !errorResponse.error.error) {
+      return throwError(error);
+    }
+
+    switch (errorResponse.error.error.message) {
+      case 'EMAIL_NOT_FOUND':
+        error.message = 'E-Mail address is not correct';
+        error.detailMessage = 'The user could have been deleted by you or an administrator';
+        error.status = 401;
+        break;
+      case 'INVALID_LOGIN_CREDENTIALS':
+        error.message = 'Password invalid or no password exists for the given email';
+        error.detailMessage = 'Please check the given email and password for correctness.';
+        error.status = 401;
+        break;
+      case 'EMAIL_EXISTS':
+        error.message = 'The email already exists';
+        error.status = 401;
+        break;
+      case 'OPERATION_NOT_ALLOWED':
+        error.message = 'You are not allowed to sign up';
+        error.status = 403;
+        break;
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+        error.message = 'Too many requests sent. Please try again later.';
+        error.status = 500;
+        break;
+      case 'USER_DISABLED':
+        error.message = 'User account not accessible';
+        error.detailMessage = 'The account have been disabled by an administrator.';
+        error.status = 403;
+        break;
+    }
+
+    return throwError(error);
   }
 }
