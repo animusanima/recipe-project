41b9b29ea8308e8960febcbd3612e009f2bad2d9
animusanima <molockokannzocko@gmail.com>
2024-06-02 08:00:40 +0200

    Added AuthModule for authorization
    - Added auth.module.ts
    - Clean up of imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,30 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgOptimizedImage} from "@angular/common";
 import {RouterLinkActive} from "@angular/router";
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {ReactiveFormsModule} from "@angular/forms";
 
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from "./app-routing/app-routing.module";
 import {AuthInterceptorService} from "./auth/auth-interceptor/auth-interceptor.service";
-import {AuthComponent} from "./auth/auth.component";
 import {HeaderComponent} from './header/header.component';
 import {RecipesModule} from "./recipes/recipes.module";
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
+import {AuthModule} from "./auth/auth.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    AuthComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     ReactiveFormsModule,
     NgOptimizedImage,
     RouterLinkActive,
     HttpClientModule,
     RecipesModule,
     ShoppingListModule,
+    AuthModule,
     AppRoutingModule,
   ],
   providers: [
---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,36 @@
+import {NgModule} from "@angular/core";
+import {AuthComponent} from "./auth.component";
+import {RouterModule, Routes} from "@angular/router";
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {SharedModule} from "../shared/shared.module";
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'auth',
+    pathMatch: 'full',
+  },
+  {
+    path: 'auth',
+    component: AuthComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/auth'
+  }
+]
+
+@NgModule({
+  declarations: [
+    AuthComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes),
+    SharedModule
+  ],
+  exports: [RouterModule]
+})
+export class AuthModule {}
